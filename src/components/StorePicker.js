9f0d95d49c5043ab7e2187dd28792c227b5cdf8d
Bret Doucette <doucette.bret@gmail.com>
2017-01-11 12:03:26 -0700

    Finished video 13

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,8 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`)
   }
 
   render() {
@@ -19,6 +20,10 @@ class StorePicker extends React.Component {
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
